6eea3586d68d6e1f55fa25d4b6e3e65e173e0bd7
Alexandre Tavares <www.dayes@gmail.com>
2019-07-08 00:53:50 +0100

    API calls working correctly. Interface concluded at 80%. Need to fix the bugs on the Password Generator controller

---
+++ PwLocker/Extensions/UIView.swift
@@ -0,0 +1,18 @@
+//
+//  UIView.swift
+//  PwLocker
+//
+//  Created by Tavares on 06/07/2019.
+//  Copyright © 2019 Tavares. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
